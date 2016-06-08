6e0500a6bbd5e2af166d6996bd70523e77f5e898
Mobile-Connect <stephen.doyle@mobile-life.com>
2016-06-08 18:09:31 +0100

    Rationalisation of second demo app

---
+++ mobile-connect-demo2/app/src/main/java/com/gsma/android/xoperatorapidemo/utils/AppSettings.java
@@ -0,0 +1,10 @@
+package com.gsma.android.xoperatorapidemo.utils;
+
+/*
+ * Customise the settings in this file in order to use your own application details
+ */
+public class AppSettings {
+
+
+    private static String discoveryClientID=
+}
